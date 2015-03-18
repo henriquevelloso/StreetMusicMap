8d80d7f448a23897f74f2aba4514226feaaa427a
Henrique Velloso <HenriqueVelloso@MacBook-Velloso.local>
2015-03-17 21:50:35 -0300

    Initial Commit

---
+++ StreetMusicMap/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StreetMusicMap
+//
+//  Created by Henrique Velloso on 3/17/15.
+//  Copyright (c) 2015 TrueTapp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StreetMusicMap/SecondViewController.m
@@ -0,0 +1,27 @@
+//
+//  SecondViewController.m
+//  StreetMusicMap
+//
+//  Created by Henrique Velloso on 3/17/15.
+//  Copyright (c) 2015 TrueTapp. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
